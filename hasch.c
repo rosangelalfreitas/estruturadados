ebae9df3fa0284bee5cfeaa0f2421da35fea36da
rosangelalfreitas <89887328+rosangelalfreitas@users.noreply.github.com>
2021-10-16 21:32:37 -0400

    Create hasch.c

---
+++ hasch.c
@@ -0,0 +1,5 @@
+Medidas Separatrizes
+
+As medidas separatrizes são os quartil (divide por quatro), decil (divide por dez), e percentil (divide por cem). Elas auxiliam a interpretação dos dados e faz separação de porcentagens da população/amostra. Vários exemplos podem ser observados no trecho da reportagem a seguir, extraído de "Metade dos brasileiros sobrevive com menos de R$ 15 por dia, aponta IBGE".
+
+Metade dos brasileiros sobrevive com apenas R$ 438 mensais, ou seja, quase 105 milhões de pessoas têm menos de R$ 15 por dia para satisfazer todas as suas necessidades básicas, segundo dados do Instituto Brasileiro de Geografia e Estatística (IBGE). Os resultados são referentes à renda média real domiciliar per capita de 2019, apurada pela Pesquisa Nacional por Amostra de Domicílios Contínua: Rendimento de todas as fontes 2019.
